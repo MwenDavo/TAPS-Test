f2ad5c6bcae6a5bff2a526cfb45ace1268443fb5
David Gillig <davidgillig9@gmail.com>
2025-11-21 19:45:21 -0300

    Testing

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from sensor.taps import TAPS
 BEAMNGTECHDIR = "C:\\Users\\David Gillig\\Downloads\\BeamNG.tech.v0.36.4.0"
 
 bng = BeamNGpy('localhost', 25252, home=BEAMNGTECHDIR, quit_on_close=True)
-bng.open(None,'-gfx', 'vk')
+bng.open(["tech/TAPS"],'-gfx', 'vk')
 
 scenario = Scenario('automation_test_track', 'Test Track')
-main_vehicle = Vehicle('main_vehicle', model='etk800', license='TEST', extensions=["tech/sensors","tech/techCore"])
+main_vehicle = Vehicle('main_vehicle', model='etk800', license='TEST')
 scenario.add_vehicle(main_vehicle, pos=(487,178,132), cling=True)
 scenario.make(bng)
 
@@ -18,17 +18,10 @@ bng.traffic.spawn(10)
 bng.scenario.start()
 main_vehicle.ai.set_mode('traffic')
 
-try:
-    taps_v1 = TAPS("TAPS", bng, main_vehicle)
-except Exception as e:
-    print(e)
-    bng.close()
+#taps_v1 = TAPS("TAPS", bng, main_vehicle)
 
 try:
    while True:
-        #print(taps_v1.poll())
-    pass
+        pass
 except KeyboardInterrupt:
-    pass
-
-bng.close()
+    bng.close()
